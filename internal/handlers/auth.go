17698a6e089c158cae3f5cde867814f4540aba4b
Thai Nguyen Khang <127386953+Nervxz@users.noreply.github.com>
2024-05-15 09:26:51 +0700

    update sign out sign in

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,13 +1,19 @@
 package handlers
 
 import (
+	"context"
+	"database/sql"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nervxz/msg-board/internal/utils"
 	"golang.org/x/crypto/bcrypt"
 )
 
-// Auth Sources https://www.youtube.com/watch?v=d4Y2DkKbxM0 9:48 - 44:30
+/* Auth Sources https://www.youtube.com/watch?v=d4Y2DkKbxM0 9:48 - 44:30
+
+ */ 
 func setupAuth(g *gin.RouterGroup, deps *dependencies) {
     h := &AuthHandler{deps: deps}
     h.bind(g)
@@ -19,7 +25,8 @@ type AuthHandler struct {
 
 func (h *AuthHandler) bind(g *gin.RouterGroup) {
     g.POST("/signup", h.signup)
-
+    g.POST("/signin", h.signin)
+    g.POST("/signout", h.signout)
 }
 
 
@@ -62,3 +69,69 @@ func (h *AuthHandler) signup(gtx *gin.Context) {
     gtx.String(http.StatusOK, "User created successfully")
 }
 
+func (h *AuthHandler) signin(gtx *gin.Context) {
+    var user struct {
+        Username string `json:"username"`
+        Password string `json:"password"`
+    }
+    if err := gtx.BindJSON(&user); err != nil {
+        gtx.String(http.StatusBadRequest, "Invalid request payload")
+        return
+    }
+
+    // Retrieve the user from the database
+    var dbUser struct {
+        UserID   int
+        Username string
+        Password string
+    }
+    err := h.deps.db.QueryRow("SELECT UserID, Username, Password FROM Users WHERE Username = $1", user.Username).Scan(&dbUser.UserID, &dbUser.Username, &dbUser.Password)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            gtx.String(http.StatusUnauthorized, "InCorrect username or password")
+            return
+        }
+        gtx.String(http.StatusInternalServerError, "Failed to query user: %v", err)
+        return
+    }
+
+    // Compare the stored hashed password with the provided password
+    if err := bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(user.Password)); err != nil {
+        gtx.String(http.StatusUnauthorized, "Invalid username or password")
+        return
+    }
+
+    // Create a session
+    sessionToken := utils.GenerateToken()
+    err = h.deps.redis.Set(context.Background(), sessionToken, dbUser.UserID, 24*time.Hour).Err()
+    if err != nil {
+        gtx.String(http.StatusInternalServerError, "Failed to create session: %v", err)
+        return
+    }
+	 // Set the session token as a cookie
+	gtx.SetCookie("session_token", sessionToken, 3600*24, "/", "", false, true)
+
+    // Return the session token
+    gtx.JSON(http.StatusOK, gin.H{"token": sessionToken})
+}
+
+
+func (h *AuthHandler) signout(gtx *gin.Context) {
+    token, err := gtx.Cookie("session_token")
+    if err != nil {
+        gtx.String(http.StatusBadRequest, "Missing authorization token")
+        return
+    }
+
+    // Delete the session
+    err = h.deps.redis.Del(context.Background(), token).Err()
+    if err != nil {
+        gtx.String(http.StatusInternalServerError, "Failed to delete session: %v", err)
+        return
+    }
+
+    // Clear the session token cookie
+    gtx.SetCookie("session_token", "", -1, "/", "", false, true)
+
+    gtx.String(http.StatusOK, "Signed out successfully")
+}
