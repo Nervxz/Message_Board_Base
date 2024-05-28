59a72b057172ccda5916d8f9b06909136adb3869
Thai Nguyen Khang <127386953+Nervxz@users.noreply.github.com>
2024-05-29 00:37:01 +0700

    unsave_adding

--- internal/dao/common.go
+++ internal/dao/common.go
@@ -5,9 +5,15 @@ import (
 	"strings"
 )
 
+// genSQLParams takes the number of records and the number of arguments per record
+// generates the SQL parameters for the query
+// numRecords: The number of records for which to generate SQL
+// sb: A pointer to a strings.Builder used to build the SQL string
+// numArgs: The number of arguments per record
 func genSQLParams(numRecords int, sb *strings.Builder, numArgs int) {
 	for i := 0; i < numRecords; i++ {
 		if i != 0 {
+			// Add a comma to separate the records
 			sb.WriteString(",")
 		}
 
